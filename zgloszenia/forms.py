72588249c4a7df78af811b10c2670b0b3e769e11
Radio2840 <kinga.smolarek1@gmail.com>
2023-12-18 10:04:45 +0100

    Stworzenie formularza do zgłoszeń
    Formularz do zgłoszeń

--- zgloszenia/forms.py
+++ zgloszenia/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 
+from zgloszenia.models import Report
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(label='login', max_length=100)
     password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ['tytul', 'opis', 'zdjecie', 'stan_zgloszenia', 'waga_zgloszenia']
+
+    def __init__(self, *args, **kwargs):
+        super(ReportForm, self).__init__(*args, **kwargs)
+        self.fields['waga_zgloszenia'].widget = forms.Select(choices=Report.IMPORTANCE_CHOICES)
+
