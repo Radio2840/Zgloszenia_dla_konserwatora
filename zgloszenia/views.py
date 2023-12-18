0144fef0abd12b784928f2e2352b2b132d56e6c5
Radio2840 <kinga.smolarek1@gmail.com>
2023-12-18 10:22:24 +0100

    Widok Formularza dla zgłoszeń
    Dodanie widoku dla formularza do zgłoszeń

--- zgloszenia/views.py
+++ zgloszenia/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from zgloszenia.forms import ReportForm
+
+
+def add_report(request):
+    if request.method == 'POST':
+        form = ReportForm(request.POST, request.FILES)
+        if form.is_valid():
+            report = form.save(commit=False)
+            report.autor = request.user
+            report.save()
+            return redirect('listOfReports')
+    else:
+        form = ReportForm()
+
+    return render(request, 'addReport.html', {'form': form})
