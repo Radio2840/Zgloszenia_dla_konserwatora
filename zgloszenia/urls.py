7bb65cfbe93611d4c75764df3c3da7f6113dcf7e
Radio2840 <kinga.smolarek1@gmail.com>
2024-01-03 15:10:29 +0100

    Dodanie widoku dla konserwatora

--- zgloszenia/urls.py
+++ zgloszenia/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from zgloszenia.views import home_view_k, create_report, user_login, logoutme, home_view_p
+from zgloszenia.views import home_view_k, create_report, user_login, logoutme, home_view_p, not_done_reports
 
 urlpatterns = [
     path('addreport/', create_report, name='addreport'),
     path('homep/addreport', create_report, name='addreport'),
-    path('homek/', home_view_k, name='homek'),
+    #path('homek/', home_view_k, name='homek'),
     path('homep/', home_view_p, name='homep'),
     path('login/', user_login, name='login'),
     path('logoutme', logoutme, name="logoutme"),
+    path('homek/', not_done_reports, name='list'),
 ]
