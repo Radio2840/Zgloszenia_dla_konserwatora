42070ec987252235b2fb0a835cf26fca829e47cb
Przemek B <pprzemek89@gmail.com>
2024-01-04 23:02:09 +0100

    Update settings.py
    allowed hosts, żeby działało online

--- konserwator/settings.py
+++ konserwator/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hvnnv7(-y=g1u_rt2#d=cr_6m-jj$=qfnpra)!84lx(-hb#2ch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
